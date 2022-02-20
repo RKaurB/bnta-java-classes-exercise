8750f8c92f9454d76824e449c88119272fd7b269
Rachel Kaur <rachel.km@outlook.com>
2022-02-20 03:47:14 +0000

    test setters

--- src/main/java/car_dealership/Main.java
+++ src/main/java/car_dealership/Main.java
@@ -10,10 +10,17 @@ public class Main {
         Car mercedes = new Car(Manufacturer.MERCEDES, 10_000.00, EngineType.HYBRID);
         // Print mercedes Car object
         System.out.println(mercedes);
+
         // Test getters
         System.out.println(mercedes.getManufacturer());
         System.out.println(mercedes.getPrice());
         System.out.println(mercedes.getEngineType());
 
+        // Test setters
+        // Double newPrice = mercedes.getPrice() / 2;
+        // mercedes.setPrice(newPrice);
+        // System.out.println(mercedes.getPrice());
+        mercedes.setPrice(mercedes.getPrice() / 2);
+        System.out.println("New price: " + mercedes.getPrice());
     }
 }
