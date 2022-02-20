6e806b42575972831dbd6bbb5abddcbe10f9f420
Rachel Kaur <rachel.km@outlook.com>
2022-02-20 03:31:33 +0000

    create new Car object and test getters in Main

--- src/main/java/car_dealership/Main.java
+++ src/main/java/car_dealership/Main.java
@@ -1,8 +1,19 @@
 package car_dealership;
 
+// Main class
 public class Main {
 
+    // Main method
     public static void main(String[] args) {
-        
+
+        // Create new instance of Car object, from Car class
+        Car mercedes = new Car(Manufacturer.MERCEDES, 10_000.00, EngineType.HYBRID);
+        // Print mercedes Car object
+        System.out.println(mercedes);
+        // Test getters
+        System.out.println(mercedes.getManufacturer());
+        System.out.println(mercedes.getPrice());
+        System.out.println(mercedes.getEngineType());
+
     }
 }
