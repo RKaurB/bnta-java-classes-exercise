8cfc40c005039f983554277aba2660f423a9f277
Rachel Kaur <rachel.km@outlook.com>
2022-02-20 03:09:29 +0000

    add getters and setters to Main class

--- src/main/java/car_dealership/Car.java
+++ src/main/java/car_dealership/Car.java
@@ -8,4 +8,39 @@ public class Car {
     Double price;
     EngineType engineType;
 
+    // Constructors
+    public Car() {
+    }
+
+    public Car(Manufacturer manufacturer, Double price, EngineType engineType) {
+        this.manufacturer = manufacturer;
+        this.price = price;
+        this.engineType = engineType;
+    }
+
+    // Getters & Setters
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public EngineType getEngineType() {
+        return engineType;
+    }
+
+    public void setEngineType(EngineType engineType) {
+        this.engineType = engineType;
+    }
+
 }
