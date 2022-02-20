ea54fff59982fe3eeb56ee4b122793900f7add26
Rachel Kaur <rachel.km@outlook.com>
2022-02-20 10:58:17 +0000

    add constructors to Dealership

--- src/main/java/car_dealership/CarDealership.java
+++ src/main/java/car_dealership/CarDealership.java
@@ -4,8 +4,20 @@ package car_dealership;
 // Contains the business logic
 public class CarDealership {
 
+    // Properties
     private String name;
     private int maxCars;
     private Car[] carsInStock;
 
+    // Constructor
+    public CarDealership() {}
+
+    public CarDealership(String name, int maxCars) {
+        this.name = name;
+        this.maxCars = maxCars;
+        // Use maxCars value to create an empty array with that number of spaces
+        this.carsInStock = new Car[maxCars];
+
+
+    }
 }
