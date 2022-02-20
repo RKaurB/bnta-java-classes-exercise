c79cbf224ea3a489710cfe5bae2dfaf60bdb4994
Rachel Kaur <rachel.km@outlook.com>
2022-02-20 02:42:49 +0000

    add Main class

--- src/main/java/cat_example/Main.java
+++ src/main/java/cat_example/Main.java
@@ -1,4 +1,20 @@
 package cat_example;
 
+// Main class
 public class Main {
+
+    // Main method
+    public static void main(String[] args) {
+
+        // Create new instance of Cat object from Cat class, passing in name property
+        Cat cat = new Cat("Larry");
+
+        // Use Getter to get cat name, and store name in the catName variable
+        String catName = cat.getName();
+
+        // Print cat name
+        System.out.println(catName);
+
+    }
+
 }
