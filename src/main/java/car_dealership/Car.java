6811eaae91cfb1a3b6038c5ba05ade68b945ef83
Rachel Kaur <rachel.km@outlook.com>
2022-02-20 03:14:11 +0000

    Revert "add getters and setters to Main class"
    This reverts commit 8cfc40c005039f983554277aba2660f423a9f277.

--- src/main/java/car_dealership/Car.java
+++ src/main/java/car_dealership/Car.java
@@ -3,39 +3,4 @@ package car_dealership;
 public class Car {
 
 
-    // Constructors
-    public Car() {
-    }
-
-    public Car(Manufacturer manufacturer, Double price, EngineType engineType) {
-        this.manufacturer = manufacturer;
-        this.price = price;
-        this.engineType = engineType;
-    }
-
-    // Getters & Setters
-    public Manufacturer getManufacturer() {
-        return manufacturer;
-    }
-
-    public void setManufacturer(Manufacturer manufacturer) {
-        this.manufacturer = manufacturer;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public EngineType getEngineType() {
-        return engineType;
-    }
-
-    public void setEngineType(EngineType engineType) {
-        this.engineType = engineType;
-    }
-
 }
